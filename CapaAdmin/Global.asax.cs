5fab2aec87de17ab0805dbdc3754afd1c2bfa3c4
unnam33d <m.menaresmelandri@gmail.com>
2023-06-22 20:09:07 -0400

    cacas

--- CapaAdmin/Global.asax.cs
+++ CapaAdmin/Global.asax.cs
@@ -17,31 +17,5 @@ namespace CapaAdmin
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
-
-        //protected void Application_Error(object sender, EventArgs e)
-        //{
-        //    Exception exception = Server.GetLastError();
-        //    Response.Clear();
-
-        //    HttpException httpException = exception as HttpException;
-
-        //    if (httpException != null)
-        //    {
-        //        // Obtén el código de error HTTP
-        //        int httpStatusCode = httpException.GetHttpCode();
-
-        //        // Realiza acciones específicas para ciertos códigos de error si lo deseas
-        //        // Por ejemplo, puedes redirigir a páginas de error personalizadas según el código de error
-
-        //        // ...
-
-        //        // Registra la excepción en el registro de errores
-        //        // Puedes usar un sistema de registro como log4net o simplemente imprimir el mensaje de error en la consola
-        //        Console.WriteLine(exception.Message);
-        //    }
-
-        //    Server.ClearError();
-        //}
-
     }
 }
