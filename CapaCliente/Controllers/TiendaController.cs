7f616458abea4f60485acc3dbe6b83f99b668a7c
unnam33d <m.menaresmelandri@gmail.com>
2023-07-16 02:52:23 -0400

    ultimo avance - ya en el pago

--- CapaCliente/Controllers/TiendaController.cs
+++ CapaCliente/Controllers/TiendaController.cs
@@ -6,6 +6,9 @@ using System.Web.Mvc;
 using CapaEntidad;
 using CapaNegocio;
 using System.IO;
+using System.Threading.Tasks;
+using System.Data;
+using System.Globalization;
 
 namespace CapaCliente.Controllers
 {
@@ -182,6 +185,68 @@ namespace CapaCliente.Controllers
         public ActionResult Pedido() {
             return View();
         }
+
+        [HttpPost]
+        public async Task<JsonResult> ProcesarPago(List<Pedidos> oListaPedido, Venta oVenta)
+        {
+
+            decimal total = 0;
+            DataTable detalle_venta = new DataTable();
+            detalle_venta.Locale = new CultureInfo("es-CL");
+
+            detalle_venta.Columns.Add("IdProducto", typeof(string));
+            detalle_venta.Columns.Add("Cantidad", typeof(int));
+            detalle_venta.Columns.Add("Total", typeof(int));
+
+
+            foreach (Pedidos oPedido in oListaPedido) {
+                decimal subtotal = oPedido.Cantidad * oPedido.oProducto.Precio;
+
+                total += subtotal;
+
+                detalle_venta.Rows.Add(new object[] {
+                    oPedido.oProducto.IdProducto,
+                    oPedido.Cantidad,
+                    subtotal
+                });
+            }
+
+            oVenta.MontoTotal = total;
+            oVenta.IdCliente = ((Cliente)Session["Cliente"]).IdCliente;
+
+            TempData["Venta"] = oVenta;
+            TempData["DetalleVenta"] = detalle_venta;
+
+            return Json(new { Status = true, Link = "/Tienda/PagoEfectuado?idTransaccion=code0001&status=true" }, JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<ActionResult> PagoEfectuado() {
+
+            string idtransaccion = Request.QueryString["idTransaccion"];
+            bool status = Convert.ToBoolean(Request.QueryString["status"]);
+
+            ViewData["Status"] = status;
+
+            if (status) {
+                Venta oVenta = (Venta)TempData["Venta"];
+
+                oVenta.Contacto = "...";
+
+                DataTable detalle_venta = (DataTable)TempData["DetalleVenta"];
+
+                oVenta.IdTransaccion = idtransaccion;
+
+                string mensaje = string.Empty;
+
+                bool respuesta = new CN_Venta().Registrar(oVenta, detalle_venta, out mensaje);
+
+                ViewData["IdTransaccion"] = oVenta.IdTransaccion;
+            }
+
+            return View();
+
+        }
+
 
 
     }
--- CapaEntidad/Venta.cs
+++ CapaEntidad/Venta.cs
@@ -10,9 +10,11 @@ namespace CapaEntidad
     {
         public int IdVenta { get; set; }
         public Cliente oCliente { get; set; }
+        public int IdCliente { get; set; }
         public int TotalProducto { get; set; }
-        public double MontoTotal { get; set; }
+        public decimal MontoTotal { get; set; }
         public string IdTransaccion { get; set; }
+        public string Contacto { get; set; }        
         public Usuario oUsuario { get; set; }
         public Pedidos oPedido { get; set; }
         public bool ValidacionPago { get; set; }
